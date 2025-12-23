b2be7c664e2be51d3bd0125a87a43d440e4305b0
Newbiedevelope <rbtjr20313@gmail.com>
2025-12-23 16:32:38 +0900

    Bean 등록 실습

---
+++ src/test/java/org/springframework/samples/petclinic/sample/SampleControllerTest.java
@@ -0,0 +1,18 @@
+package org.springframework.samples.petclinic.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+public class SampleControllerTest {
+
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Test
+	public void testDI(){
+		SampleController bean = applicationContext.getBean(SampleController.class);
+	}
+}
